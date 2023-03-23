9745f7f336784a9ce819b08144f09774402a6c29
CJ Tyler <ctyler4@ufl.edu>
2023-03-22 22:52:48 -0400

    this is my first addition to the repo

---
+++ lab_06.py
@@ -0,0 +1,19 @@
+prompt_continue = True
+while prompt_continue:
+    menu = "Menu\n------------\n1. Encode\n2. Decode\n3. Quit\n"
+    print(menu)
+    option = int(input("Please enter an option: "))
+    if option == 1:
+        original = int(input("Please enter your password to encode: "))
+        print("Your password has been encoded and stored!")
+        encode = original + 33333333
+        option_1 = True
+        print(menu)
+        option = int(input("Please enter an option: "))
+        while option_1:
+            if option == 2:
+                print(f"The encoded password is {encode}, and the original password is {original}.")
+                print(menu)
+                option = int(input("Please enter an option: "))
+    if option == 3:
+        prompt_continue = False
